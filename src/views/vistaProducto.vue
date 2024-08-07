b31116ac80a5026ec57578e247e0556837469633
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-08-06 23:02:53 -0600

    filtro , componente de producto y vista del formulario hecho

---
+++ src/views/vistaProducto.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+        <productPureb  />
+
+        
+
+
+
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import productPureb  from '@/components/productPureb.vue';
+
+export default defineComponent({
+    name: 'pageVistaProducto',
+    components: {
+        productPureb
+    }
+})
+</script>
