066898e8158490c53e24a804d110e64e7f8604e3
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-08-08 23:13:09 -0600

    agregue un temporizador al carruselprincipalPage

--- src/components/carruselprincipalPage.vue
+++ src/components/carruselprincipalPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative w-full   ">
+    <div class="relative w-full">
         <div class="overflow-hidden relative">
             <div class="flex transition-transform duration-500"
                 :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
@@ -9,18 +9,18 @@
             </div>
         </div>
         <button @click="prevSlide"
-            class="absolute top-1/2 left-0 transform -translate-y-1/2  p-2 rounded-full">
+            class="absolute top-1/2 left-0 transform -translate-y-1/2 p-2 rounded-full">
             <img src="../assets/icon/arrow_left_38dp_E8EAED_FILL0_wght400_GRAD0_opsz40.png" alt="">
         </button>
         <button @click="nextSlide"
-            class="absolute top-1/2 right-0 transform -translate-y-1/2 s p-2 rounded-full">
+            class="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 rounded-full">
             <img src="../assets/icon/arrow_right_38dp_E8EAED_FILL0_wght400_GRAD0_opsz40.png" alt="">
         </button>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue';
 
 export default defineComponent({
     name: 'Carousel',
@@ -48,6 +48,13 @@ export default defineComponent({
                 currentIndex.value = 0;
             }
         };
+
+        onMounted(() => {
+            const interval = setInterval(nextSlide, 7000);
+            onUnmounted(() => {
+                clearInterval(interval);
+            });
+        });
 
         return { currentIndex, prevSlide, nextSlide };
     }
