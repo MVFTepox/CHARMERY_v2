d72421d01e1aa292d9c077842819d9a25bb2ffc3
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-08-15 19:43:13 -0600

    api cambios

--- src/Utils/api.ts
+++ src/Utils/api.ts
@@ -1,3 +1,5 @@
+import type { NumericLiteral } from "typescript";
+
 const urlApi = 'http://3.134.108.48:3333/api'
 
 async function apiRequest(endpoint: string, method: string = 'GET', data?: any) {
@@ -43,7 +45,7 @@ export function fetchProducts() {
   return apiRequest('/products');
 }
 
-export function fetchProduct(id: string) {
+export function fetchProduct(id: number) {
   return apiRequest(`/products/${id}`);
 }
 
@@ -55,11 +57,11 @@ export function fetchDetailCart() {
   return apiRequest('/detail-cart');
 }
 
-export function fetchDeliveryAddress() {
+export function fetchDeliveryAddress(id: number) {
   return apiRequest('/delivery-address');
 }
 
-export function fetchDefaultAddress() {
+export function fetchDefaultAddress(id: number) {
   return apiRequest('/default-address');
 }
 
@@ -83,6 +85,14 @@ export function postWishlist(data: any) {
   return apiRequest('/favorites', 'POST', data);
 }
 
+export function postDetailCart(data: any) {
+  return apiRequest('/detail-cart', 'POST', data);
+}
+
 export function deletewishlist(data: any) {
   return apiRequest('/favorites', 'DELETE', data);
+}
+
+export function deleteCart(data: any) {
+  return apiRequest('/carts', 'DELETE', data);
 }
